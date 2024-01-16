8f7e4d39dc5fa3fad9ee080eaab0892dc3fad2e3
Yandi <yandivd@gmail.com>
2024-01-16 15:57:14 -0500

    querysets in eloquent with controller

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use  App\Models\Curso;
+use  App\Models\User;
 
 class CursoController extends Controller
 {
     public function index(){
-        return "Index page";
+        $contador = 0;
+        //obtener los cursos  cuya categoria es 'Desarrollo web' ordenados por id descendentemente
+        $cursos = Curso::where('category','Desarrollo Web')->orderBy('id','desc')->get();
+
+        //Obtener todos los usuarios
+        $users = User::all();
+
+        $primer_curso = Curso::where('category','Diseño Web')->first();
+
+        //Recorrer todos los usuarios e incrementar un contador por cada uno de ellos
+        for ($i = 0; $i < count($users); $i++){
+            //Imprimir el usuario en cuestion
+            // printf($users[$i]);
+            //Incrementar el contador
+            $contador++;
+            //Imprimir el valor del contador
+            // print($contador);
+        }
+        //Retornar el array de usuarios
+        return $primer_curso;
     }
 
     public function create(){
