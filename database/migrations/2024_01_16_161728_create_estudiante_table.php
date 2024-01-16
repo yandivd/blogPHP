89c3c1517ec87b452ca1af12e712a9f467fe345b
Yandi <yandivd@gmail.com>
2024-01-16 11:37:50 -0500

    init to seeders

--- database/migrations/2024_01_16_161728_create_estudiante_table.php
+++ database/migrations/2024_01_16_161728_create_estudiante_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cursos', function (Blueprint $table) {
+        Schema::create('estudiantes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->text('description');
-            $table->text('category');
+            $table->integer('edad');
+            $table->boolean('feu');
         });
     }
 
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('curso');
+        Schema::dropIfExists('estudiante');
     }
 };
