c4b4d0b5f1fcf7cffb6d3ec1a5f25525dd3d011d
Yandi <yandivd@gmail.com>
2024-01-17 15:44:40 -0500

    herencia templates

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listado de los Cursos</title>
-</head>
-<body>
-    <h1>Listado de los  Cursos</h1>
-</body>
-</html>
+<!-- Herencia de plantillas -->
+@extends('layouts.plantilla')
+
+<!-- Modificar la seccion de titulo -->
+@section('title', 'Listado de plantillas')
+
+<!-- Modificar la seccion del contenido(body) -->
+@section('content')
+    <h1>Listado de las plantillas new</h1>
+@endsection
