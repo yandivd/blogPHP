1b18137844af5d9ca43092824810bb4c4f9ebd8c
Yandi <yandivd@gmail.com>
2024-01-18 08:52:24 -0500

    little style with tailwindui

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -5,8 +5,12 @@
 @section('title', 'Listado de plantillas')
 
 <!-- Modificar la seccion del contenido(body) -->
+@section('head')
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900">Listado de Cursos</h1>
+@endsection
+
 @section('content')
-    <h1>Listado de las plantillas new</h1>
+    {{-- <h1>Listado de las plantillas new</h1> --}}
     <ul>
         @foreach ($cursos_a_mostrar as $i)
             <li>{{$i->name}}</li>
